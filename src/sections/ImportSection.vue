fe0d6f5aaceab952dd816c605569bf90ec2afde1
itmir913 <itmir913@gmail.com>
2026-04-22 22:38:21 +0900

    ImportSection, 열 이름 별칭 추가

--- src/sections/ImportSection.vue
+++ src/sections/ImportSection.vue
@@ -9,8 +9,8 @@ const COL_ALIASES = {
   classNum: ['반', 'class', '학급', '반번호', 'classnum', 'class_num'],
   number: ['번호', 'number', 'num', '번', '출석번호'],
   name: ['이름', 'name', '성명', '학생명', '학생이름'],
-  activityName: ['활동명', '활동', '분류', 'activity', 'activity_name', 'activityname'],
-  activityContent: ['활동내용', '내용', 'content', '기록', '활동기록'],
+  activityName: ['활동명', '활동 명', '활동', '분류', 'activity', 'activity_name', 'activityname'],
+  activityContent: ['활동내용', '활동 내용', '내용', 'content', '기록', '활동기록'],
   studentId: ['학번', 'studentid', 'student_id'],
 }
 
